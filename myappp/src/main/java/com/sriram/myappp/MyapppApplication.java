36a16743b0b9f8443440b222b74f1ba1d8d9aa76
Sriram Kodipaka <kodipakasriram@gmail.com>
2026-04-25 15:12:26 +0530

    feat: initial Spring Boot project

---
+++ myappp/src/main/java/com/sriram/myappp/MyapppApplication.java
@@ -0,0 +1,13 @@
+package com.sriram.myappp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyapppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyapppApplication.class, args);
+	}
+
+}
---
+++ myappp/src/test/java/com/sriram/myappp/MyapppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sriram.myappp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyapppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
